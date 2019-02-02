fa6c3fa17a54ac0b2b0a5cb12cd6ebb3ef5b7d87
Parth <psareen@uwaterloo.ca>
2019-02-02 18:23:20 -0500

    update test app

--- testApp.py
+++ testApp.py
@@ -1,11 +1,20 @@
-from flask import Flask
-    
-    app = Flask(__name__)
-    
-    @app.route('/')
-        def index():
-            return 'Hello, Flask!'
-        
-        if __name__ == '__main__':
-        	app.run(debug=True)
+#app.py
 
+from flask import Flask, request #import main Flask class and request object
+
+app = Flask(__name__) #create the Flask app
+
+@app.route('/query-example')
+def query_example():
+    return 'Todo...'
+
+@app.route('/form-example')
+def formexample():
+    return 'Todo...'
+
+@app.route('/json-example')
+def jsonexample():
+    return 'Todo...'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000) #run app in debug mode on port 5000
