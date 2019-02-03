e41c59e8b39117e146067598a69a3524cd6f3ebc
Alex Jabbour <redpod22@gmail.com>
2019-02-02 23:55:57 -0500

    Add files via upload

---
+++ FrontEnd/app.js
@@ -0,0 +1,10 @@
+var DockerApp = angular.module('DockerApp', []);
+
+angular.module('myApp', [])
+  .controller('myCtrl', ['$scope', function($scope) {
+    $scope.count = 0;
+   	$scope.
+    $scope.myFunc = function() {
+      $scope.count++;
+    };
+  }]);
